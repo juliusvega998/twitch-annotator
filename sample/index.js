463c5cf53c8e3803430aabd8e2e4f1bdab46ff00
juliusvega998 <juliusvega998@gmail.com>
2017-01-29 23:07:27 +0800

    Added raw chat messages

--- sample/index.js
+++ sample/index.js
@@ -7,35 +7,36 @@ const fs = require('fs');
 const dict = require(__dirname + '/dictionary')
 
 const url = 'https://rechat.twitch.tv/rechat-messages?start=TIME&video_id=vVIDID';
-const video_id = '111312545';
+const video_ids = ['97690975','83400929','116549596'];
 
 const MAX_ITER = 10000;
 const TIME_OUT = 500;
 const INCREMENT = 30;
-const FILE_NAME = 'nightblue3.txt';
+const FILE_NAME = [__dirname + '/raw/lol.txt', __dirname + '/raw/dota2.txt', __dirname + '/raw/csgo.txt'];
 
 let msgs = [];
 let done = false;
 
 const start = () => {
-	fs.writeFile(FILE_NAME, '');
-
-	gatherTimePeriod();
-	extract(0);
+	for(let i=0; i<3; i++) {
+		fs.writeFile(FILE_NAME[i], '');
+		gatherTimePeriod(i);
+		extract(0, i);
+	}
 }
 
-const gatherTimePeriod = () => {
+const gatherTimePeriod = (i) => {
 	request
-		.get(url.replace("VIDID", video_id).replace("TIME", 0))
+		.get(url.replace("VIDID", video_ids[i]).replace("TIME", 0))
 		.end((err, res) => {
 			if(err){
 				let toks = res.body.errors[0].detail.split(' ');
-				gatherMsgs(parseInt(toks[4]), parseInt(toks[6]), 0);
+				gatherMsgs(parseInt(toks[4]), parseInt(toks[6]), 0, i);
 			}
 		});
 }
 
-const gatherMsgs = (i, end, iter) => {
+const gatherMsgs = (i, end, iter, id) => {
 	if(i >= end) {
 		done = true;
 	} else if(iter >= MAX_ITER) {
@@ -43,59 +44,30 @@ const gatherMsgs = (i, end, iter) => {
 		console.log("Maximum tries reached.");
 	} else {
 		request
-			.get(url.replace("VIDID", video_id).replace("TIME", i))
+			.get(url.replace("VIDID", video_ids[id]).replace("TIME", i))
 			.end((err, res) => {
 				if(err){
 					if(res) {
-						console.log(res.body.errors[0].detail);
+						console.log(res.body.errors[0].detail + ' ' + id);
 					} else {
 						console.log(err);
 					}
 					setTimeout(() => {
-						gatherMsgs(i, end, iter+1);
+						gatherMsgs(i, end, iter+1, id);
 					}, TIME_OUT);
 				} else {
 					msgs = msgs.concat(getMessage(res.body));
-					gatherMsgs(i+INCREMENT, end, 0);
+					gatherMsgs(i+INCREMENT, end, 0, id);
 				}
 			});
 	}
-}
-
-const gatherMsg = (i, end, iter) => {
-	request
-		.get(url.replace("VIDID", video_id).replace("TIME", i))
-		.end((err, res) => {
-			if(err){
-				if(res) {
-					console.log(res.body.errors[0].detail);
-				} else {
-					console.log(err);
-				}
-				setTimeout(() => {
-					gatherMsgs(i, end, iter+1);
-				}, TIME_OUT);
-			} else {
-				let msgs2 = getMessage(res.body);
-				if(msgs2) {
-					msgs = msgs.concat(msgs2);
-					fs.appendFile('time.txt', "\n", function(err) {
-						if(err) {
-							console.log(err);
-						}
-					});
-				}
-
-				done = true;
-			}
-		});
 }
 
 const getMessage = (body) => {
 	let msg = [];
 
 	for(let i=0; i < body.data.length; i++) {
-		if(!body.data[i].attributes.deleted) {
+		if(!body.data[i].attributes.deleted || body.data[i].attributes.from !== 'moobot') {
 			msg.push(body.data[i].attributes.message);
 		}
 	}
@@ -103,33 +75,35 @@ const getMessage = (body) => {
 	return msg;
 }
 
-const extract = (i) => {
+const extract = (i, file) => {
 	if(msgs.length) {
 		let msg = msgs.shift();
-		process(msg);
-		extract(i+1);
+
+		process(msg, file);
+		extract(i+1, file);
 	} else {
 		if(!done) {
 			setTimeout(() => {
-				extract(i);
+				extract(i, file);
 			}, TIME_OUT);
 		}
 	}
 }
 
-const process = (msg) => {
-	//preprocess(msg);
-	fs.appendFile(FILE_NAME, msg + '\n', function(err) {
+const process = (msg, file) => {
+	preprocess(msg, file);
+	/*fs.appendFile(FILE_NAME[file], msg + '\n', function(err) {
 		if(err) {
-			console.log(err);
+			console.log(err)
 		} else {
 			console.log(msg)
 		}
-	});
+	});*/
 }
 
-const preprocess = (msg) => {
+const preprocess = (msg, file) => {
 	let usernamePattern = /@[A-Za-z0-9]+/g;
+	let commandPattern = /![A-Za-z0-9]+/g;
 	let urlPattern = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g;
 	let nonAlphaNumPattern = /[^a-zA-Z0-9 ]+/g;
 
@@ -137,7 +111,19 @@ const preprocess = (msg) => {
 	let str = "";
 
 	msg = removePattern(msg, usernamePattern);
-	msg = removePattern(msg, urlPattern);
+	msg = removePattern(msg, commandPattern);
+
+	if(msg) {
+		fs.appendFile(FILE_NAME[file], msg + '\n', function(err) {
+			if(err) {
+				console.log(err)
+			} else {
+				console.log(msg)
+			}
+		});
+	}
+
+	/*msg = removePattern(msg, urlPattern);
 	msg = toLower(msg);
 
 	msg = changeAbbrev(msg);
@@ -147,7 +133,7 @@ const preprocess = (msg) => {
 	msg = removeNounAndArticles(msg);
 	msg = normalize(msg);
 
-	str = "new: " + msg + "\nold: " + oldMsg + "\n\n";
+	str = "new: " + msg + "\nold: " + oldMsg + "\n\n";*/
 
 	/*fs.appendFile('message.txt', str, function(err) {
 		if(err) {
