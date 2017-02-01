ca24ee0a4267cf7ae9277d50a519251d9ec1283c
juliusvega998 <juliusvega998@gmail.com>
2017-02-01 21:54:27 +0800

    Improved code on selecting initial raw data

--- sample/form.js
+++ sample/form.js
@@ -2,32 +2,26 @@
 
 const fs = require('fs');
 
-const BULLDOG = 'chat_messages/bulldog.txt';
-const NIGHTBLUE3 = 'chat_messages/nightblue3.txt';
-const ADREN = 'chat_messages/adren.txt';
-const FILE_NAME = 'chats.txt';
+const input = [__dirname + '/raw/csgo.txt', __dirname + '/raw/dota2.txt', __dirname + '/raw/lol.txt'];
+const output = [__dirname + '/selected/csgo.txt', __dirname + '/selected/dota2.txt', __dirname + '/selected/lol.txt'];
+const chat_number = 500
 
 const start = () => {
-	let bulldog = fs.readFileSync(BULLDOG, 'utf-8').split('\n');
-	let nightblue3 = fs.readFileSync(NIGHTBLUE3, 'utf-8').split('\n');
-	let adren = fs.readFileSync(ADREN, 'utf-8').split('\n');
+	let raw = [];
 
-	fs.writeFile(FILE_NAME, '');
+	for(let i=0; i<3; i++) {
+		raw.push(fs.readFileSync(input[i], 'utf-8').split('\n'));
+		fs.writeFile(output[i], '');
+	}
 
-	for(let i=0; i<1000; i++) {
-		let index = 0;
-
-		index = Math.floor(Math.random() * bulldog.length);
-		fs.appendFileSync(FILE_NAME, bulldog[index] + '\n');
-		bulldog.splice(index, 1);
-
-		index = Math.floor(Math.random() * nightblue3.length);
-		fs.appendFileSync(FILE_NAME, nightblue3[index] + '\n');
-		nightblue3.splice(index, 1);
-
-		index = Math.floor(Math.random() * adren.length);
-		fs.appendFileSync(FILE_NAME, adren[index] + '\n');
-		adren.splice(index, 1);
+	for(let i=0; i<chat_number; i++) {
+		for(let j=0; j<3; j++) {
+			let index = 0;
+			
+			index = Math.floor(Math.random() * raw[j].length);
+			fs.appendFileSync(output[j], raw[j][index] + '\n');
+			raw[j].splice(index, 1);
+		}
 	}
 
 	console.log('done!');
