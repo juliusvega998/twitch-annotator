c69a99f2fba63345b0239733e6297c96f2470707
juliusvega998 <juliusvega998@gmail.com>
2017-02-05 10:41:22 +0800

    fixed duplicate chat messages

--- sample/form.js
+++ sample/form.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 const input = [__dirname + '/raw/csgo.txt', __dirname + '/raw/dota2.txt', __dirname + '/raw/lol.txt'];
 const output = [__dirname + '/selected/csgo.txt', __dirname + '/selected/dota2.txt', __dirname + '/selected/lol.txt'];
 const chat_number = 500
+let msgs = [];
 
 const start = () => {
 	let raw = [];
+
+	console.log("started!");
 
 	for(let i=0; i<3; i++) {
 		raw.push(fs.readFileSync(input[i], 'utf-8').split('\n'));
@@ -19,12 +22,28 @@ const start = () => {
 			let index = 0;
 			
 			index = Math.floor(Math.random() * raw[j].length);
-			fs.appendFileSync(output[j], raw[j][index] + '\n');
-			raw[j].splice(index, 1);
+			if(checkAdded(raw[j][index])) {
+				raw[j].splice(index, 1);
+				j--;
+			} else {
+				fs.appendFileSync(output[j], raw[j][index] + '\n');
+				msgs.push(raw[j][index]);
+				raw[j].splice(index, 1);
+			}
 		}
 	}
 
 	console.log('done!');
+}
+
+const checkAdded = (msg) => {
+	for(let i=0; i<msgs.length; i++) {
+		if(msg.toLowerCase() === msgs[i].toLowerCase()) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 start();
